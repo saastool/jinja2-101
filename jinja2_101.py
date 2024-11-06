9125759199403d645a19f5706f11f7ccd32484c6
Justin Wang <justin17728@gmail.com>
2024-11-06 16:56:21 +0800

    Ready the material

--- jinja2_101.py
+++ jinja2_101.py
@@ -1,11 +1,16 @@
+# 1. Import the necessary libraries and components (objects, functions, etc.) from libraries.
 from jinja2 import Environment, FileSystemLoader, Template
 
+# 2. Create a jinja rendering environment and store it in a variable. This environment will be used in further steps.
 env = Environment(loader = FileSystemLoader('template'))
-template = env.get_template('variables_template.jinja')
+# 3. Load the template (which stored in templates) in a variable.
+template = env.get_template('_template.jinja')
 
-output = template.render(name = 'World')
+# 4. Render the template using <template-object>.render() function to obtain text.
+output = template.render()
 
+# 5. Print the rendered text to the screen or a file as suitable and output into file in the renders folder.
 print(output)
 
-with open("renders/variables_output.txt", 'w') as f:
+with open("renders/_output.txt", 'w') as f:
     print(output, file = f)
