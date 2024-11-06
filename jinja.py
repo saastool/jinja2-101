786531c2a3b0dc3e0f0d15e4e7c353402a8da925
Justin Wang <justin17728@gmail.com>
2024-11-06 17:55:06 +0800

    This is how your repository look like after finished turtorial

---
+++ jinja.py
@@ -0,0 +1,17 @@
+# 1. Import the necessary libraries and components (objects, functions, etc.) from libraries.
+from jinja2 import Environment, FileSystemLoader, Template
+
+# 2. Create a jinja rendering environment and store it in a variable. This environment will be used in further steps.
+env = Environment(loader = FileSystemLoader('template'))
+# 3. Load the template (which stored in templates) in a variable.
+template = env.get_template('macro_template.jinja')
+
+# 4. Render the template using <template-object>.render() function to obtain text.
+from datetime import datetime
+output = template.render(date=datetime.now())
+
+# 5. Print the rendered text to the screen or a file as suitable and output into file in the renders folder.
+print(output)
+
+with open("renders/macro_output.txt", 'w') as f:
+    print(output, file = f)
